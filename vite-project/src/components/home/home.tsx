18a7b7a053dc9ce091d640397046957bdbd099de
kevito112 <kevinlang9981@gmail.com>
2024-09-16 14:22:12 -0400

    Added routing

---
+++ vite-project/src/components/home/home.tsx
@@ -0,0 +1,12 @@
+import './home.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Welcome to the Home Component</h1>
+            <p>This is the home page of the application.</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ vite-project/src/components/services/services.tsx
@@ -0,0 +1,12 @@
+import './services.css';
+
+const Home = () => {
+    return (
+        <div className="services">
+            <h1>Welcome to the Services Component</h1>
+            <p>This is the Services of the application.</p>
+        </div>
+    );
+};
+
+export default Home;

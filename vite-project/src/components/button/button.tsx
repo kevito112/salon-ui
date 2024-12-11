d6484d42d839a74e4920e262d01b761f2f8bfac7
kevito112 <kevinlang9981@gmail.com>
2024-12-11 00:36:14 -0500

    Added booksy age for booking.

--- vite-project/src/components/button/button.tsx
+++ vite-project/src/components/button/button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
     url?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, style, className, url = 'https://booksy.com/en-us/1369477_key-beauty-key-biscayne_brows-lashes_15888_key-biscayne?do=invite&utm_medium=social_post_creator }) => {
+const Button: React.FC<ButtonProps> = ({ text, style, className, url = 'https://booksy.com/en-us/1369477_key-beauty-key-biscayne_brows-lashes_15888_key-biscayne?do=invite&utm_medium=social_post_creator' }) => {
 
     const handleClick = () => {
         window.location.href = url;
